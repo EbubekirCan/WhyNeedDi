c09414feaf249b3802fa7e21393ee26fbd75ea36
Ebubekir <ebubekirc442@gmail.com>
2023-06-02 00:10:27 +0300

    commit

---
+++ src/com/tpe/repository/DbRepository.java
@@ -0,0 +1,11 @@
+package com.tpe.repository;
+
+import com.tpe.domain.Message;
+
+public class DbRepository implements Repo{
+
+    @Override
+    public void save(Message message) {
+        System.out.println("Mesajınız DB ye kaydediliyor. Mesajınız: "+message.getMessage());
+    }
+}
---
+++ src/com/tpe/repository/Repo.java
@@ -0,0 +1,9 @@
+package com.tpe.repository;
+
+import com.tpe.domain.Message;
+
+public interface Repo {
+
+    void save(Message message);
+
+}
