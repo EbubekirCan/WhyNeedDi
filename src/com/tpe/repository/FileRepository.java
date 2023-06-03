c68465f33bbaf52eadd0adcf09ffb550cae15acc
Ebubekir <ebubekirc442@gmail.com>
2023-06-04 00:09:08 +0300

    commit11

---
+++ src/com/tpe/repository/FileRepository.java
@@ -0,0 +1,11 @@
+package com.tpe.repository;
+
+import com.tpe.domain.Message;
+
+public class FileRepository implements Repo{
+
+    @Override
+    public void save(Message message) {
+        System.out.println("Mesajınız dosyaya kaydediliyor. Mesajınız: "+message.getMessage());
+    }
+}
